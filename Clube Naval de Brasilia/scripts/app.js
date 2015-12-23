5693cf25be19b0a9c7ca4a991da416284a383a0e
Elson Carlos Silva de Jesus <elsoncarlos@gmail.com>
2015-12-23 22:58:06 +0000

    Changes made by Elson Carlos Silva de Jesus (elsoncarlos@gmail.com).

--- Clube Naval de Brasilia/scripts/app.js
+++ Clube Naval de Brasilia/scripts/app.js
@@ -37,13 +37,109 @@
         
         // comment out the following line to get a UI which matches the look
         // and feel of the operating system
-        skin: 'flat',
+        skin: 'nova',
 
         // the application needs to know which view to load first
         initial: 'views/home.html'
       });
 
     }, false);
+    
+    function onDeviceReady() {
+     window.analytics.Start();
+     // existing code goes here
+	};
+
+	function onPause() {
+     window.analytics.Stop();
+     // existing code goes here
+	};
+	function onResume() {
+     window.analytics.Start();
+     // existing code goes here
+	};
 
 
 }());
+
+
+(function (g) {
+
+	var productId = "41cdc9e2125244fb9bb2455355d54b10"; // App unique product key
+
+	// Make analytics available via the window.analytics variable
+	// Start analytics by calling window.analytics.Start()
+	var analytics = g.analytics = g.analytics || {};
+	analytics.Start = function () {
+		// Handy shortcuts to the analytics api
+		var factory = window.plugins.EqatecAnalytics.Factory;
+		var monitor = window.plugins.EqatecAnalytics.Monitor;
+		// Create the monitor instance using the unique product key for Analytics
+		var settings = factory.CreateSettings(productId);
+		settings.LoggingInterface = factory.CreateTraceLogger();
+		factory.CreateMonitorWithSettings(settings,
+			function () {
+				console.log("Monitor created");
+				// Start the monitor inside the success-callback
+				monitor.Start(function () {
+					console.log("Monitor started");
+				});
+			},
+			function (msg) {
+				console.log("Error creating monitor: " + msg);
+			}
+		);
+	};
+	analytics.Stop = function () {
+		var monitor = window.plugins.EqatecAnalytics.Monitor;
+		monitor.Stop();
+	};
+	analytics.Monitor = function () {
+		return window.plugins.EqatecAnalytics.Monitor;
+	};
+})(window);
+
+function onDeviceReady() {
+	var app;
+	app = new Application();
+	app.run();
+}
+
+function Application() {
+}
+
+Application.prototype = {
+
+	run: function() {
+		var that = this,
+			openLocal = document.getElementById("openLocal"),
+			openExternalInAppBrowser = document.getElementById("openExternalInAppBrowser"),
+			openExternalPDF = document.getElementById("openExternalPDF"),
+			openExternalInSystemBrowser = document.getElementById("openExternalInSystemBrowser");
+
+			openLocal.addEventListener("click", that.openLocal);
+			openExternalInAppBrowser.addEventListener("click", that.openExternalInAppBrowser);
+			openExternalPDF.addEventListener("click", that.openExternalPDF);
+			openExternalInSystemBrowser.addEventListener("click", that.openExternalInSystemBrowser);
+	},
+
+	openLocal: function() {
+		window.open("img/ice.png", "_blank");
+	},
+ 
+	openExternalInAppBrowser:  function () {
+		window.open("http://icenium.com", "_blank");
+	},
+
+	openExternalPDF:  function () {
+		if (window.navigator.simulator === true) {
+			alert("Not Supported in Simulator.");
+		} else {
+			window.open("http://www.telerik.com/whitepapers/appbuilder/approaching-mobile-understanding-the-three-ways-to-build-mobile-apps", "_blank");
+		}
+	},
+
+	openExternalInSystemBrowser:function () {
+		window.open("http://wiki.apache.org/cordova/InAppBrowser", "_system");
+	}
+}
---
+++ Clube Naval de Brasilia/scripts/kendo.custom.js
@@ -0,0 +1,65 @@
+// Kendo UI theme for data visualization widgets
+// Use as theme: 'newTheme' in configuration options (or change the name)
+kendo.dataviz.ui.registerTheme('newTheme', {
+    "chart": {
+        "title": {
+            "color": "#32364c"
+        },
+        "legend": {
+            "labels": {
+                "color": "#32364c"
+            }
+        },
+        "chartArea": {},
+        "seriesDefaults": {
+            "labels": {
+                "color": "#32364c"
+            }
+        },
+        "axisDefaults": {
+            "line": {
+                "color": "#dfe0e1"
+            },
+            "labels": {
+                "color": "#32364c"
+            },
+            "minorGridLines": {
+                "color": "#dfe0e1"
+            },
+            "majorGridLines": {
+                "color": "#dfe0e1"
+            },
+            "title": {
+                "color": "#32364c"
+            }
+        },
+        "seriesColors": [
+            "#ff4350",
+            "#0070c0",
+            "#00acc1",
+            "#80deea",
+            "#ffbf46",
+            "#8a8a8a"
+        ]
+    },
+    "gauge": {
+        "pointer": {
+            "color": "#ff4350"
+        },
+        "scale": {
+            "rangePlaceholderColor": "#dfe0e1",
+            "labels": {
+                "color": "#32364c"
+            },
+            "minorTicks": {
+                "color": "#32364c"
+            },
+            "majorTicks": {
+                "color": "#32364c"
+            },
+            "line": {
+                "color": "#32364c"
+            }
+        }
+    }
+});
